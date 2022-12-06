d7bdb4bca045791399c880b35dc1cd0711500082
Svetlana <knsveta@mail.ru>
2022-12-06 10:14:42 +0300

    lesson_5

---
+++ lesson5/index.js
@@ -0,0 +1,69 @@
+//Используя наработки практического задания прошлого урока, создайте веб-версию приложения.
+//Сделайте так, чтобы при запуске она:
+//● показывала содержимое текущей директории;
+//● давала возможность навигации по каталогам из исходной папки;
+//● при выборе файла показывала его содержимое.
+
+import http from "http";
+import fs from "fs";
+import path from "path";
+import { Transform } from "stream";
+
+const host = "localhost";
+const port = 5000;
+
+const list = [];
+const fsp = fs.promises;
+
+const links = (arr, curUrl) => {
+  if (curUrl.endsWith("/")) curUrl = curUrl.substring(0, curUrl.length - 1);
+  let li = "";
+  for (const item of arr) {
+    li += `<li><a href="${curUrl}/${item}">${item}</a></li>`;
+  }
+  return li;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    const url = req.url.split("?")[0];
+    const curPath = path.join(process.cwd(), url);
+
+    fs.stat(curPath, (err, stats) => {
+      if (!err) {
+        if (stats.isFile(curPath)) {
+          const rs = fs.createReadStream(curPath, "utf-8");
+          rs.pipe(res);
+        } else {
+          fsp
+            .readdir(curPath)
+            .then((files) => {
+              if (url !== "/") files.unshift("..");
+              return files;
+            })
+            .then((data) => {
+              // render
+              const filePath = path.join(process.cwd(), "./index.html");
+              const rs = fs.createReadStream(filePath);
+              const ts = new Transform({
+                transform(chunk, encoding, callback) {
+                  const li = links(data, url);
+                  this.push(chunk.toString().replace("#file#", li));
+
+                  callback();
+                },
+              });
+
+              rs.pipe(ts).pipe(res);
+            });
+        }
+      } else {
+        res.end("Path not exists");
+      }
+    });
+  }
+});
+
+server.listen(port, host, () =>
+  console.log(`Server running at http://${host}:${port}`)
+);
